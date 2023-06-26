27b725932bee86e85dea4a9669936fe8a3e201bf
michaelsinanta <michaelchristlambert@gmail.com>
2023-06-26 18:30:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Getting Started
+clone repository
+```bash
+npm install
+npm run dev
+```
+website bisa diakses di localhost:3000
+taro image atau external di public/images
+kalo mau buat module atau elements atau sisipin icon baru bisa liat dokumentasi dari stride https://github.com/DJaegerScript/next-stride/blob/main/DOCUMENTATION.md
+kalo mau nambah warna atau font size di tailwind.config.js
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'poppins',
+        poppinsMedium: 'poppins-medium',
+      },
+      colors: {
+        lightGreen: '#87BE57',
+        darkGreen: '#458549',
+        paleGreen: '#C0D493',
+        mintGreen: '#D6E5BE',
+        seaGreen: '#5DC597',
+        teal: '#62BB96',
+        coral: '#EA8C60'
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
