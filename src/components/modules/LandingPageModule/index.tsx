f67099d2c0cfaab7dc251925f6e4851614029761
michaelsinanta <michaelchristlambert@gmail.com>
2023-07-03 22:25:31 +0700

    fix: rename

--- src/components/modules/LandingPageModule/index.tsx
+++ src/components/modules/LandingPageModule/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import { HeroModule } from './sections/hero'
-import { ActionModule } from './sections/action'
-import { WalkingText } from './sections/walking'
 import { FaqModule } from './sections/Faq'
 
 export const LandingPageModule: React.FC = () => {
   return (
     <>
       <div className="flex flex-col ">
-        <HeroModule />
-        <ActionModule />
-        <WalkingText />
         <FaqModule />
       </div>
     </>
